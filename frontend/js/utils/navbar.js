0c63bf5a6aef7e463c240154b1e3e08dcd368bb0
EsterPis <esteratlante@gmail.com>
2026-05-21 00:26:50 +0200

    badge carrello ok

--- frontend/js/utils/navbar.js
+++ frontend/js/utils/navbar.js
@@ -1,3 +1,22 @@
+// aggiorna badge carrello cliente
+export function updateCartBadge() {
+    const badge = document.getElementById("cart-count");
+    if (!badge) return;
+
+    const cart = JSON.parse(localStorage.getItem("cart")) || null;
+    if (!cart || !cart.items.length) {
+        badge.textContent = 0;
+        return;
+    }
+
+    const totalItems = cart.items.reduce(
+        (sum, item) => sum + item.quantity,
+        0
+    );
+
+    badge.textContent = totalItems;
+}
+
 // renderizza la navbar con stili dinamici in base al ruolo dell'utente (restaurateur o cliente)
 export function renderNavbar() {
     const role = localStorage.getItem("role");
@@ -30,4 +49,5 @@ export function renderNavbar() {
     `;
 
     document.body.insertAdjacentHTML("afterbegin", navbarHTML); // Inserisce la navbar all'inizio del body
+    updateCartBadge(); // Aggiorna il badge del carrello al caricamento della pagina
 }
